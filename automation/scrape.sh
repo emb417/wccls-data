67f49051356018184f835242ec1d279cadac6d6f
Eric Brousseau <emb417@gmail.com>
2017-08-21 01:24:56 -0700

    timestamp log files

--- automation/scrape.sh
+++ automation/scrape.sh
@@ -1,15 +1,41 @@
 #!/usr/bin/env bash
 
-working_dir='/Users/eric/github/wccls-data'
+# Define a timestamp function
+ts() {
+  date +"%s"
+}
+
 unholdables=('ghost%20in%20the%20shell' 'logan')
+un_size='10'
+
 onshelves=('ps4' 'bluray')
+os_size='50'
+
+branch='39'
+avail_codes=(
+  'In'
+  'Out'
+  'Lost'
+  'Missing'
+  'Transferred'
+  'In-Transit'
+  'Held'
+  '-- Not Holdable'
+  'On-Order'
+  'In-Repair'
+  'Unavailable')
+
+protocol='http'
+host='127.0.0.1'
+port='1337'
+working_dir='/Users/eric/github/wccls-data'
 
 for u in ${unholdables[@]}; do
-  curl http://127.0.0.1:1337/\?size\=10\&keyword\=${u} -o $working_dir/logs/${u}.json
+  curl $protocol://$host:$port/\?size\=$un_size\&keyword\=${u} -o $working_dir/logs/${u}__$(ts).json
 done
 
 for o in ${onshelves[@]}; do
-  curl http://127.0.0.1:1337/\?branch\=39\&size\=500\&keyword\=${o} -o $working_dir/logs/${o}.json
+  curl $protocol://$host:$port/\?branch\=$branch\&size\=$os_size\&keyword\=${o} -o $working_dir/logs/${o}__$(ts).json
 done
 
 exit 0
