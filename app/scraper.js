d9dec5c7f4301ad1cdf8af3df8f8173915429334
Eric Brousseau <eric.brousseau@nike.com>
2017-08-28 03:10:33 -0700

    writing db as file and formatting msg

--- app/scraper.js
+++ app/scraper.js
@@ -4,37 +4,35 @@ const config = require('../config/global.json');
 
 // return urls to crawl for availability data
 const availabilityUrls = ( context, response ) => {
-  // console.log('exec availabilityUrls...', context);
   const urls = [];
   // use branchId if supplied in context otherwise search all
   const branchIds = (typeof context.branch != "undefined" ) ?
     [ context.branch ] : config.branchIds;
   // construct urls for all items and branches combos
-  // console.log('branchIds', branchIds);
   for ( const itemId of parser.getIds( response.data ) ) {
     for ( const branchId of branchIds ) {
       urls.push( `${ config.baseUrl }/Items/1.${ itemId }.1.${ branchId }.1` );
     };
   }
-  // console.log('return availabilityUrls', urls);
+  
   return urls;
 };
 
 exports.scrape = ( keyword, context ) => {
   return new Promise( (resolve, reject) => {
-    // console.log(keyword, context);
+    console.log("getting search results for keyword: ", keyword);
     // search by keyword to get ids
     axios.get( `${ config.baseUrl }/Results/?ls=1.${ context.resultsSizeLimit || '10' }.0.&t=${ keyword }` )
       .then( response => {
-        // console.log('baseUrl response', response);
+        console.log('get availabilityUrls...');
         // build array from concurrent requests per item and branch combo
         let promiseArray = availabilityUrls( context, response ).map( url => {
           return axios.get( url );
         });
-        // console.log('promiseArray', promiseArray);
         // once all concurrent requests are complete, parse results per response
         axios.all(promiseArray.map(p => p.catch(() => undefined)))
         .then( results => {
+          console.log("filtering availability...");
           let filteredAvailability = [];
           results.map( r => {
             const availability = parser.getAvailability( r.data, context );
@@ -44,10 +42,8 @@ exports.scrape = ( keyword, context ) => {
             }
             return;
           });
-          let formattedResult = ( context.type==="msg" && filteredAvailability[0] )
-            ? `${ filteredAvailability[0].title.replace(/\s/g, '.') }::${ filteredAvailability[0].branch.replace(/\s/g, '.') }`
-            : filteredAvailability;
-          resolve(formattedResult);
+
+          resolve(filteredAvailability);
         })
       })
       .catch( error => { reject(error) } );
--- app/unhold.js
+++ app/unhold.js
@@ -7,29 +7,68 @@ const unholdConfig = require('../config/unhold.json');
 const unhold = Express();
 
 unhold.use((req, res) => {
+  console.log("...setting initial context...");
   const context = { 
     availabilityCode: unholdConfig.availabilityCode,
-    resultsSizeLimit: unholdConfig.resultsSizeLimit,
-    type: unholdConfig.type
+    resultsSizeLimit: unholdConfig.resultsSizeLimit
   };
-  
+  console.log("building promise array...");
   let promises = [];
   unholdConfig.keywords.forEach( keyword => {
     promises.push(scraper.scrape(keyword, context));
   });
-  // console.log(promises);
+  console.log("promise all...");
   Promise.all(promises.map(p => p.catch(() => undefined)))
   .then( results => {
     const timestamp = Date.now();
     // console.log(timestamp, results);
-    
-    const obj = {
-      "unholdables": {
-        "timestamp": timestamp,
-         results  
-      }
+    console.log("adding timestamps...");
+    const promiseData = {
+      "timestamp": timestamp,
+      "items": results  
     };
-    res.send(obj);
+    console.log("checking items.db...");
+    fs.access('./data/items.db', fs.constants.F_OK, (err) => {
+      // if db doesn't exist, create it
+      if(err){
+        console.log("items.db does not exist, writing a new one...");
+        const unholdableData = { "unholdables": [ promiseData ] };
+        fs.writeFile('./data/items.db', JSON.stringify(unholdableData), (err) => {
+            if (err) { throw err; }
+            console.log("items.db initialized...");
+            return;
+        });
+      } else {
+        console.log("items.db does exists, appending new data...");
+        fs.readFile('./data/items.db', 'utf8', (err, fileData) => {
+          if (err) { throw err; }
+          // read and write same, soon merge...
+          let unholdables = JSON.parse(fileData).unholdables;
+          unholdables.push(promiseData);
+          const mergedData = { unholdables };
+          // write over items.db with newly merged data
+          fs.writeFile('./data/items.db', JSON.stringify(mergedData), (err) => {
+              if (err) { throw err; }
+              console.log("items.db updated...");
+              return;
+          });
+        });
+      };
+    });
+    let formattedData = promiseData;
+    if(unholdConfig.type === "msg"){
+      console.log("formatting results for msg...");
+      formattedData = "";
+      promiseData.items.forEach( branchTitles => {
+        // console.log("branchTitlestitles: ", branchTitles);
+        branchTitles.forEach( branchTitle => {
+          if(branchTitle.items.includes(`In -- ${ unholdConfig.availabilityCode }`)){
+            formattedData += `${ branchTitle.title.replace(/\s/g, '.') }::${ branchTitle.branch.replace(/\s/g, '.') } | `;
+          }
+        });
+      });      
+    }
+    res.send(formattedData);
   });
 });
 
